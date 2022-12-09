552bbcd304904a3bab538265977ab17650af65f1
Evgymart <alignant@yandex.ru>
2022-12-09 09:43:14 +0300

    Install redis, setting up. Work in progress

--- backend/main.go
+++ backend/main.go
@@ -1,10 +1,43 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/go-redis/redis/v8"
 )
+
+func redisTest() {
+	ctx := context.Background()
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	err := rdb.Set(ctx, "key", "value", 0).Err()
+	if err != nil {
+		panic(err)
+	}
+
+	val, err := rdb.Get(ctx, "key").Result()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("key", val)
+
+	val2, err := rdb.Get(ctx, "key2").Result()
+	if err == redis.Nil {
+		fmt.Println("key2 does not exist")
+	} else if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("key2", val2)
+	}
+}
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the HomePage!")
@@ -17,5 +50,6 @@ func handleRequests() {
 }
 
 func main() {
+	redisTest()
 	handleRequests()
 }
