733e68716e60c601c9592c1f9fd61a37925f7b0c
Evgymart <alignant@yandex.ru>
2022-12-11 22:06:02 +0300

    Added a store class to work with urls

---
+++ backend/db/storage.go
@@ -0,0 +1,33 @@
+package db
+
+import "math/rand"
+
+func ReadFullUrl(db Database, shortUrl string) (string, error) {
+	val, err := db.Client.Get(Ctx, shortUrl).Result()
+	if err != nil {
+		return "", err
+	}
+
+	return val, nil
+}
+
+func WriteFullUrl(db Database, fullUrl string) (string, error) {
+	shortUrl := generateRandomUrl()
+	err := db.Client.Set(Ctx, shortUrl, fullUrl, 0).Err()
+	if err != nil {
+		return "", err
+	}
+
+	return shortUrl, nil
+}
+
+func generateRandomUrl() string {
+	const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	const size = 16
+	bytes := make([]byte, size)
+	for i := range bytes {
+		bytes[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+
+	return string(bytes)
+}
--- backend/main.go
+++ backend/main.go
@@ -1,43 +1,16 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"log"
 	"net/http"
-
-	"github.com/go-redis/redis/v8"
+	"shorturl/backend/db"
 )
 
-func redisTest() {
-	ctx := context.Background()
-
-	rdb := redis.NewClient(&redis.Options{
-		Addr:     "redis:6379",
-		Password: "", // no password set
-		DB:       0,  // use default DB
-	})
-
-	err := rdb.Set(ctx, "key", "value", 0).Err()
-	if err != nil {
-		panic(err)
-	}
-
-	val, err := rdb.Get(ctx, "key").Result()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("key", val)
-
-	val2, err := rdb.Get(ctx, "key2").Result()
-	if err == redis.Nil {
-		fmt.Println("key2 does not exist")
-	} else if err != nil {
-		panic(err)
-	} else {
-		fmt.Println("key2", val2)
-	}
-}
+var (
+	ListenAddr = ":80"
+	RedisAddr  = "redis:6379"
+)
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the HomePage!")
@@ -46,10 +19,10 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 
 func handleRequests() {
 	http.HandleFunc("/", homePage)
-	log.Fatal(http.ListenAndServe(":80", nil))
+	log.Fatal(http.ListenAndServe(ListenAddr, nil))
 }
 
 func main() {
-	redisTest()
+	db.NewDatabase(RedisAddr)
 	handleRequests()
 }
